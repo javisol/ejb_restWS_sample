875c716bcd9896ab18d7c4f43669c73db3aa49e9
Javier Solabre <javier.solabre@thomsonreuters.com>
2023-04-25 14:41:40 +0200

    fix url stateless

--- src/main/java/org/ratoncio/stateless/EjbStatelessLocalServlet.java
+++ src/main/java/org/ratoncio/stateless/EjbStatelessLocalServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
-@WebServlet(urlPatterns= "/stateless")
+@WebServlet(urlPatterns= "/statelesslocal")
 public class EjbStatelessLocalServlet extends HttpServlet{
 
     private static final long serialVersionUID = 1L;
